ab6c761d10d81bd789b58f5a9c3fe619e0e15ac2
oyiooi <15055142531@163.com>
2019-04-01 03:43:19 +0800

    plan-a

--- src/fliter.js
+++ src/fliter.js
@@ -1,27 +1,31 @@
 import React,{Component} from 'react';
 
 class Fliter extends Component {
-    render() {
+
+  /*showinfowindow(place){
+    let locationmarkers=this.props.markerlocation;
+    let infowindow=new this.props.google.maps.InfoWindow();
+    for(let i=0;i<locationmarkers.length;i++){
+      if(locationmarkers[i].title===place.title){
+        locationmarkers[i].populateInfoWindow (locationmarkers[i].marker, infowindow)
+      }
+    }
+    
+  }*/
+
+  render() {
+    let placese=this.props.placese;
         if(this.props.showing){
             return (
                 <div id='cloumn1'>
                   <header className="App-header">Street Map</header>
                   <div className='fliter-container'>
-                    <select className='fliter' id='fliter'>
-                      <option value='1'>1</option>
-                      <option value='1'>1</option>
-                      <option value='1'>1</option>
-                      <option value='1'>1</option>
-                      <option value='1'>1</option>
+                    <select className='fliter' id='fliter' value={this.props.currentCate} onChange={(event)=>{this.props.updateCurrentCata(event)}} >
+                    {this.props.catagra.map((item)=><option id={item.index} value={item}>{item}</option>)}
                     </select>
-                    <button className='fliter-button'>Fliter</button>
+                    <button className='fliter-button' onClick={this.props.updateplace}>Fliter</button>
                      <ul className='place-list'>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
+                        {placese.map((place)=><li id={place.title} value={place.title}>{place.title}</li>)}
                      </ul>
                   </div>
                 </div>
